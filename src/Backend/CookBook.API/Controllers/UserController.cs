b955eb45fd685c10da56d3d84dbf6f33c4a123d2
Eryálef Vieira <eryalef@gmail.com>
2024-10-16 14:09:53 -0300

    feat: adding endpoint to register an user

---
+++ src/Backend/CookBook.API/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using CookBook.Communication.Requests;
+using CookBook.Communication.Responses;
+
+namespace CookBook.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpPost]
+        [ProducesResponseType(typeof(ResponseRegisteredUserJson), StatusCodes.Status201Created)]
+        public IActionResult Register(RequestRegisterUserJson request)
+        {
+            return Created();
+        }
+    }
+}
