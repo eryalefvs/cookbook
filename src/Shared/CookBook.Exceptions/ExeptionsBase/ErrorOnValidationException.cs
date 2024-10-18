a4ddaa4224c4927c391ea4f801a8eb691816263f
Eryálef Vieira <eryalef@gmail.com>
2024-10-18 19:54:25 -0300

    feat: adding exceptions and exceptions messages to register user

---
+++ src/Shared/CookBook.Exceptions/ExeptionsBase/ErrorOnValidationException.cs
@@ -0,0 +1,9 @@
+﻿namespace CookBook.Exceptions.ExeptionsBase
+{
+    public class ErrorOnValidationException : CookBookExceptions
+    {
+        public IList<string> ErrorMessages { get; set; }
+
+        public ErrorOnValidationException(IList<string> errorMessages) { ErrorMessages = errorMessages; }
+    }
+}
