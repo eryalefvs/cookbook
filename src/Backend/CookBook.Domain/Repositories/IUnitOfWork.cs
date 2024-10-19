5a273858ca1aee4e7e400ded16579e0186eeaa70
Eryálef Vieira <eryalef@gmail.com>
2024-10-19 14:20:05 -0300

    feat: adding user repository

---
+++ src/Backend/CookBook.Domain/Repositories/IUnitOfWork.cs
@@ -0,0 +1,7 @@
+﻿namespace CookBook.Domain.Repositories
+{
+    public interface IUnitOfWork
+    {
+        public Task Commit();
+    }
+}
---
+++ src/Backend/CookBook.Domain/Repositories/User/IUserWriteOnlyRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace CookBook.Domain.Repositories.User
+{
+    public interface IUserWriteOnlyRepository
+    {
+        public Task Add(Entities.User user);
+    }
+}
